c261efd3a5769092cc0016661c53978b1e05cc7f
Tina <TinaNguyen290@yahoo.com>
2018-10-01 17:32:11 -0700

    Completed Tutorial 1
    https://www.youtube.com/watch?v=MwZwr5Tvyxo

--- FlaskBlog.py
+++ FlaskBlog.py
@@ -2,5 +2,17 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-	return "Hello World!"
+@app.route("/home")
+def home():
+	return "Home"
+	
+@app.route("/about")
+def about():
+	return "About"
+	
+	
+
+#Automatically turns debug on
+#With this you can use "python FlaskBlog.py" instead of "flask run"
+if __name__ == '__main__':
+	app.run(debug=True)
