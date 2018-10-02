364243e560e9f84bd42764ccef392a0b95752bb6
Tina <TinaNguyen290@yahoo.com>
2018-10-01 19:29:46 -0700

    Created Login/Register forms

--- FlaskBlog.py
+++ FlaskBlog.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegisterationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'ceca7ffa42e3d3b78e6ca32add09c505'
 
 posts =[
 	{
@@ -29,7 +32,18 @@ def home():
 def about():
 	return render_template('about.html', title='About')
 	
-	
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+	form = RegisterationForm()
+	if form.validate_on_submit():
+		flash('Account created!', 'success')
+		return redirect(url_for('home'))
+	return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+	form = LoginForm()
+	return render_template('login.html', title='Login', form=form)	
 
 #Automatically turns debug on
 #With this you can use "python FlaskBlog.py" instead of "flask run"
